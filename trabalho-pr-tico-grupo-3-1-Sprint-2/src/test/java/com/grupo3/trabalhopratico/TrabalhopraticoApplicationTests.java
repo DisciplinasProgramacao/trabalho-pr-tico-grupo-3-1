2b892f3db468427f49914bfaf5452ff6e12532d6
gabrielnog2 <145882683+gabrielnog2@users.noreply.github.com>
2024-05-15 22:21:57 -0300

    Update TrabalhopraticoApplicationTests.java

--- trabalho-pr-tico-grupo-3-1-Sprint-2/src/test/java/com/grupo3/trabalhopratico/TrabalhopraticoApplicationTests.java
+++ trabalho-pr-tico-grupo-3-1-Sprint-2/src/test/java/com/grupo3/trabalhopratico/TrabalhopraticoApplicationTests.java
@@ -5,9 +5,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrabalhopraticoApplicationTests {
-
+@Before
+    public void setUp() {
+        requisicao = new Requisicao(3, 2, 1, 1); // Total de 3 mesas: 2 de 4 lugares, 1 de 6 lugares, 1 de 8 lugares
+    }
 	@Test
 	void contextLoads() {
 	}
+  @Test
+    public void testFazerReserva() {
+       
+        requisicao.fazerReserva(4);
+        assertEquals(1, requisicao.getMesas().get(0).getQuantidadeReservas());  
+        requisicao.fazerReserva(5);
+        assertEquals(1, requisicao.getFilaDeEspera().size());
+    }
 
+	public void testConcluirReserva() {
+        requisicao.fazerReserva(4);
+        requisicao.concluirReserva(4);
+        assertEquals(0, requisicao.getMesas().get(0).getQuantidadeReservas());
+        requisicao.fazerReserva(6);
+        requisicao.concluirReserva(6);
+        assertEquals(0, requisicao.getMesas().get(1).getQuantidadeReservas());
+    }
+
+ @Test
+    public void testExibirMesas() {
+        requisicao.fazerReserva(4);
+        requisicao.fazerReserva(6);
+        requisicao.fazerReserva(8);
+        requisicao.fazerReserva(5); 
+        requisicao.exibirMesas();
+    }
+	
 }
